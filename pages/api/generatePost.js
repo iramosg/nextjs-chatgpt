3f31706a84910e29f90c1aac67a9ed117474bca2
Igor Ramos <igor.ramos@live.com>
2023-03-18 19:24:16 -0300

    Usando a versão gpt-3.5-turbo

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -12,24 +12,24 @@ export default async function handler(req, res) {
   // const keywords =
   //   'first-time dog owners, common dog health issues, best dog breeds'
 
-  const response = await openai.createCompletion({
-    model: 'text-davinci-003',
-    temperature: 0,
-    max_tokens: 3600,
-    prompt: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords ${keywords}.
-    The content should be formatted in SEO-friendly HTML.
-    The response must also include appropriate HTML title and meta description content.
-    The return format must ben stringified JSON in the following format:
-    {
-      "postContent": post content here,
-      "title": title goes here,
-      "metaDescription": meta description goes here
-    }`,
-  })
+  // const response = await openai.createCompletion({
+  //   model: 'text-davinci-003',
+  //   temperature: 0,
+  //   max_tokens: 3600,
+  //   prompt: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords ${keywords}.
+  //   The content should be formatted in SEO-friendly HTML.
+  //   The response must also include appropriate HTML title and meta description content.
+  //   The return format must ben stringified JSON in the following format:
+  //   {
+  //     "postContent": post content here,
+  //     "title": title goes here,
+  //     "metaDescription": meta description goes here
+  //   }`,
+  // })
 
   // const post = JSON.parse(response.data.choices[0]?.text.split('\n').join(''))
 
-  const response2 = await openai.createChatCompletion({
+  const response = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     max_tokens: 3600,
     temperature: 0,
@@ -53,9 +53,9 @@ export default async function handler(req, res) {
     ],
   })
 
-  const post = JSON.parse(response2.data?.choices[0]?.message.content)
+  const post = JSON.parse(response.data?.choices[0]?.message.content)
 
-  console.log('response:', response2)
+  console.log('response:', response)
 
   res.status(200).json({
     post,
