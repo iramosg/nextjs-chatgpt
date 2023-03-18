78c3d54635f3795e905f02f819226e983327de51
Igor Ramos <igor.ramos@live.com>
2023-03-18 13:56:53 -0300

    Sidebar footer

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,4 +1,9 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { useUser } from '@auth0/nextjs-auth0/client'
+
 export const AppLayout = ({ children }) => {
+  const { user } = useUser()
   return (
     <div>
       <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
@@ -11,7 +16,29 @@ export const AppLayout = ({ children }) => {
           <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
             List of Posts
           </div>
-          <div className="bg-cyan-800">User information</div>
+          <div className="bg-cyan-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2">
+            {!!user ? (
+              <>
+                <div className="min-w-[50px]">
+                  <Image
+                    src={user.picture}
+                    alt={user.name}
+                    height={50}
+                    width={50}
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="flex-1">
+                  <div className="font-bold">{user.email}</div>
+                  <Link href="/api/auth/logout" className="text-sm">
+                    Logout
+                  </Link>
+                </div>
+              </>
+            ) : (
+              <Link href="/api/auth/login">Login</Link>
+            )}
+          </div>
         </div>
 
         <div className="">{children}</div>
