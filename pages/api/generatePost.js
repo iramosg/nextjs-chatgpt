6152225ffc930dabc6110ea88c37cec47d50bbf9
Igor Ramos <igor.ramos@live.com>
2023-03-18 19:14:34 -0300

    Recebendo as informações do formulário

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -6,9 +6,11 @@ export default async function handler(req, res) {
   })
   const openai = new OpenAIApi(config)
 
-  const topic = 'Top 10 tips for dog  owners'
-  const keywords =
-    'first-time dog owners, common dog health issues, best dog breeds'
+  const { topic, keywords } = req.body
+
+  // const topic = 'Top 10 tips for dog owners'
+  // const keywords =
+  //   'first-time dog owners, common dog health issues, best dog breeds'
 
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
@@ -27,9 +29,7 @@ export default async function handler(req, res) {
 
   console.log('response:', response)
 
-  res
-    .status(200)
-    .json({
-      post: JSON.parse(response.data.choices[0]?.text.split('\n').join('')),
-    })
+  res.status(200).json({
+    post: JSON.parse(response.data.choices[0]?.text.split('\n').join('')),
+  })
 }
--- pages/post/new.js
+++ pages/post/new.js
@@ -4,12 +4,20 @@ import { useState } from 'react'
 
 export default function NewPost() {
   const [post, setPost] = useState('')
-  const handleClick = async () => {
+  const [topic, setTopic] = useState('')
+  const [keywords, setKeywords] = useState('')
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
     const response = await fetch('/api/generatePost', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
+      body: JSON.stringify({
+        topic,
+        keywords,
+      }),
     })
     const json = await response.json()
     console.log(json.post.postContent)
@@ -18,10 +26,30 @@ export default function NewPost() {
 
   return (
     <div>
-      <h1>New Post Page</h1>
-      <button className="btn" onClick={handleClick}>
-        Generate
-      </button>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            <strong>Generate a blog post on the topic of:</strong>
+            <textarea
+              value={topic}
+              onChange={(e) => setTopic(e.target.value)}
+              className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            <strong>Targeting the following keywords:</strong>
+          </label>
+          <textarea
+            value={keywords}
+            onChange={(e) => setKeywords(e.target.value)}
+            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+          />
+        </div>
+        <button className="btn">Generate</button>
+      </form>
+
       <div
         dangerouslySetInnerHTML={{ __html: post }}
         className="max-w-screen-sm p-10"
