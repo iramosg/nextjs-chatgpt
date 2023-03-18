be5e18f0e71f4df8efd963cfa6ae70c4fb0dff82
Igor Ramos <igor.ramos@live.com>
2023-03-18 18:50:27 -0300

    Integração inicial com a api OpenAI

--- pages/post/new.js
+++ pages/post/new.js
@@ -2,9 +2,23 @@ import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import { AppLayout } from '../../components/AppLayout'
 
 export default function NewPost() {
+  const handleClick = async () => {
+    const response = await fetch('/api/generatePost', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const json = await response.json()
+    console.log(json)
+  }
+
   return (
     <div>
       <h1>New Post Page</h1>
+      <button className="btn" onClick={handleClick}>
+        Generate
+      </button>
     </div>
   )
 }
