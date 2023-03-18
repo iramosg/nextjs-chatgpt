8de2e9d11b25cb1e452776a8254f54b44396c723
Igor Ramos <igor.ramos@live.com>
2023-03-18 13:50:18 -0300

    Sidebar com Tailwind

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,9 +1,21 @@
 export const AppLayout = ({ children }) => {
   return (
     <div>
-      <main>
-        App Layout <div>{children}</div>
-      </main>
+      <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
+        <div className="flex flex-col text-white overflow-hidden">
+          <div className="bg-slate-800">
+            <div>Logo</div>
+            <div>CTA</div>
+            <div>Tokens</div>
+          </div>
+          <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
+            List of Posts
+          </div>
+          <div className="bg-cyan-800">User information</div>
+        </div>
+
+        <div className="">{children}</div>
+      </div>
     </div>
   )
 }
