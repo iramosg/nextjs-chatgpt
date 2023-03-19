e0a082a821a9d5c609faf2569d1e3de27f3cc0cb
Igor Ramos <igor.ramos@live.com>
2023-03-19 12:39:37 -0300

    Utilizando webhooks do stripe

--- pages/api/addTokens.js
+++ pages/api/addTokens.js
@@ -1,16 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { getSession } from '@auth0/nextjs-auth0'
-import clientPromise from '../../lib/mongodb'
 import stripeInit from 'stripe'
 
 const stripe = stripeInit(process.env.STRIPE_SECRET_KEY)
 
 export default async function handler(req, res) {
   const { user } = await getSession(req, res)
-  console.log(user)
-  const client = await clientPromise
-  const db = client.db('blogstandard')
 
   const lineItems = [
     {
@@ -27,24 +23,15 @@ export default async function handler(req, res) {
     line_items: lineItems,
     mode: 'payment',
     success_url: `${protocol}${host}/success`,
+    payment_intent_data: {
+      metadata: {
+        sub: user.sub,
+      },
+    },
+    metadata: {
+      sub: user.sub,
+    },
   })
-
-  const userProfile = await db.collection('users').updateOne(
-    {
-      auth0Id: user.sub,
-    },
-    {
-      $inc: {
-        avaliableTokens: 10,
-      },
-      $setOnInsert: {
-        auth0Id: user.sub,
-      },
-    },
-    {
-      upsert: true,
-    }
-  )
 
   res.status(200).json({ session: checkoutSession })
 }
