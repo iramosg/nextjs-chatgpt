9cc03a7bc7974e89954e025a224505934952556c
Igor Ramos <igor.ramos@live.com>
2023-03-19 13:51:47 -0300

    Criando e implementando o método getPosts

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -15,7 +15,7 @@ export const AppLayout = ({
 }) => {
   const { user } = useUser()
 
-  const { setPostsFromSSR, posts } = useContext(PostsContext)
+  const { setPostsFromSSR, posts, getPosts } = useContext(PostsContext)
 
   useEffect(() => {
     setPostsFromSSR(postsFromSSR)
@@ -46,7 +46,12 @@ export const AppLayout = ({
                 {post.topic}
               </Link>
             ))}
-            <div className="hover:underline text-sm text-slate-400 text-center cursor-pointer mt-4">
+            <div
+              onClick={() => {
+                getPosts({ lastPostDate: posts[posts.length - 1].created })
+              }}
+              className="hover:underline text-sm text-slate-400 text-center cursor-pointer mt-4"
+            >
               Load more posts
             </div>
           </div>
